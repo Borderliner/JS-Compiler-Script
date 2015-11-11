719f08fd00670706dc1344cf8a97f93dd0d85f91
Psyringe <psyringe@yandex.com>
2015-11-11 23:49:20 +0330

    Fixed equal sign issue. Fixed FILE_NAME variable

--- js_compile.sh
+++ js_compile.sh
@@ -1,13 +1,13 @@
 #! /usr/bin/bash
 
-FILES = "*.js"
+FILES="*.js"
 
 function compile()
 {
 	for f in $FILES
 	do
-		OUTPUT_FILE = $f"c"
-		INPUT_FILE = $f
+		OUTPUT_FILE=$f"c"
+		INPUT_FILE=$f
 		echo "Compiling \""$INPUT_FILE"\""
 		closure --js $INPUT_FILE --js_output_file $OUTPUT_FILE
 	done
@@ -30,11 +30,11 @@ function prompt()
 
 function replace()
 {
-	C_FILES = "*.jsc"
+	C_FILES="*.jsc"
 	for f in $C_FILES
 	do
 		echo "Replacing old files with new ones..."
-		FILE_NAME = $f | cut -d'.' -f 1
+		FILE_NAME=$(echo $f | cut -d'.' -f 1)
 		mv $f $FILE_NAME".js"
 	done
 }
